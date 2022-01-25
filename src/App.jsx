4c830ef6dcc38e49f323ac7ded892f39a6f603a9
Anubhab Patnaik <anubhabr50@gmail.com>
2022-01-25 17:26:40 +0530

    Changed font and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './Components/Navbar'
 import Footer from './Components/Footer'
 import Landingsegment from './Components/Landingsegment'
+import Boxes from './Components/Boxes'
 
 function App () {
   return (
     <>
       <Header />
       <Landingsegment />
+      <Boxes />
       <Footer />
     </>
   )
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,24 +1,19 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
 
 export default function Footer () {
   return (
     <>
-      <Card text='white' className='text-center' bg='dark'>
-        <Card.Body>
-          <Card.Title />
-          <Card.Text>
-            <p>  This website is created by CodeBotx.</p>
-            <p className='lead'> Copyright &copy; 2021 Anubhab Patnaik</p>
-          </Card.Text>
-        </Card.Body>
-        <div>
-          <a href='# ' className='position-absolute bottom-0 end-0 p-2 ' position='absolute'>
-            <i className='bi bi-arrow-up-circle h1 ' />
-          </a>
+      <footer class='p-5 bg-dark text-white text-center position-relative' id='learnaboutus'>
+        <div class=' container '>
+          <p>This website is created by CodeBotx.</p>
         </div>
+        <p class='lead '>Copyright &copy; 2021 Anubhab Patnaik</p>
 
-      </Card>
+        <a href='# ' class='position-absolute bottom-0 end-0 p-5 '>
+          <i class='bi bi-arrow-up-circle h1 ' />
+        </a>
+
+      </footer>
 
     </>
   )

c657f3fbbea893c1a48d279925b7fbeb99c4e2bf
Anubhab Patnaik <anubhabr50@gmail.com>
2022-01-25 16:42:02 +0530

    adding nav, footer and landing segment componenet

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css'
 import 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './Components/Navbar'
+import Footer from './Components/Footer'
+import Landingsegment from './Components/Landingsegment'
 
 function App () {
   return (
     <>
       <Header />
+      <Landingsegment />
+      <Footer />
     </>
   )
 }

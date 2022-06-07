32e8d8a38819ae3bdbd3d5b9fe1fba76acaa8f97
Anubhab Patnaik <anubhabr50@gmail.com>
2022-06-07 10:18:03 +0530

    changed landing segment

--- src/Components/Landingsegment.js
+++ src/Components/Landingsegment.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import notes from '../assets/img/notes.svg'
 import undraw_programming_2svr from '../assets/img/undraw_programming_2svr.svg'
-
 export default function Landingsegment () {
   return (
     <>
@@ -11,7 +11,7 @@ export default function Landingsegment () {
               <h1>Grab your <span class='text-secondary'>NOTES</span> here </h1>
               <p class='lead my-4 mx-2'>Hi! Welcome to Notes.SIT. This website has been created by students of Silicon Institute of Technology, Bhubaneswar for the student community to help with notes for free!</p>
             </div>
-            <img className='img-fluid w-50 d-none d-sm-block' src={undraw_programming_2svr} alt='' />
+            <img className='img-fluid w-50 d-none d-sm-block' src={notes} alt='' />
           </div>
         </div>
       </section>

0ea46f28627c5a5d4fecdb1e36132127f0006904
fuzzymfx <anubhabr50@gmail.com>
2023-03-14 07:43:15 +0530

    Updated homepage

--- src/Components/Landingsegment.js
+++ src/Components/Landingsegment.js
@@ -20,18 +20,18 @@ export default function Landingsegment () {
 				<div className='px-3'>
 					<div className="container">
 						<p>
-						RESOC is the coolest academic notes-sharing platform around, built by a bunch of tech-savvy students at Silicon Institute.It's totally free ❤️. You don't have to sell your soul to afford good quality notes anymore. <br></br>
+						RESOC is the coolest academic notes-sharing platform around, built by a bunch of tech-savvy students at Silicon Institute, and it's totally free ❤️. You don't have to sell your soul to afford good quality notes anymore. <br></br>
 						Get started <b><a href="/notes" className=' fw text-var'> here</a></b>.
 							</p>
 							<p>
-						We have a nifty <a className='text-var' href='/chat-room'>task section</a> for you to manage your work.  No more forgetting deadlines and drowning in assignments. Phew!
+						We have a nifty <a className='text-var' href='/chat-room'>task section</a> for you to manage your work.  No more forgetting deadlines and drowning in assignments.
 							Pop over to the <a className='text-var' href='/chat-room'>community chat page</a> and give us a holler. We're always happy to help out and connect with like-minded folks. You might even make a new study buddy or two :)
 							</p>
 							<p>
 							
 							Oh, and if you really dig what we're doing here at RESOC, we'd be over the moon if you'd consider sponsoring us. Just head over to the <a href='/aboutus' className='text-var'>about section</a> and donate to our talented content creators. Trust us, it really helps keep the site running and lets us add all sorts of fun new features. Plus, you get to be a part of making education accessible for everyone. Win-win, right? ❤️
 							
-							<div className='sponsor py-3'>
+							<div className='sponsor py-2'>
 								<a href='https://www.buymeacoffee.com/anubhabr500' className='	' target='_blank' rel='noreferrer'>
 									<img src='https://cdn.buymeacoffee.com/buttons/v2/default-white.png' alt='Buy Me A Coffee' style={{height: '30px', width: '108px'}} />
 								</a>
--- src/notes/Notes.js
+++ src/notes/Notes.js
@@ -1,10 +1,97 @@
 import React from 'react'
 import Footer from '../Components/Footer'
 import Header from '../Components/Navbar'
+import moon from '../assets/img/undraw_on_the_office_fbfs.svg'
+import '../assets/css/dashboard.css'
 export default function Notes() {
 	return (
 		<>
 		<Header />
+			<div className=''>
+      <section className=" pt-5 px-5 cdin">
+				<div className="container ">
+					<div className="d-sm-flex align-items-center justify-content-between mainc">
+						<div className="img-home">
+							<h1 className="heading">NOTES<span></span></h1>
+							<p className="lead my-4">
+								Get started with your notes here
+							</p>
+						</div>
+						<img className="img-fluid w-50 d-none d-sm-block" src={moon} alt="in office" />
+					</div>
+				</div>
+			</section>
+				<div className='p-5'>
+					<div className="container">
+						<p>
+							<ul style={{
+								listStyleType: 'none',
+							}}>
+								<li><a href='engmath1' className='text-var'>Engineering Maths I</a></li>
+								<li><a href='engmath2' className='text-var'>Engineering Maths II</a></li>
+								<li><a href='engphy' className='text-var'>Engineering Physics</a></li>
+								<li><a href='engchem' className='text-var'>Engineering Chemistry</a></li>
+								<li><a href='engi' className='text-var'>English I</a></li>
+								<li><a href='engii' className='text-var'>English II</a></li>
+								<li>
+									<a href='basic-electronics' className='text-var'>Basic Electronics</a>
+								</li>
+								<li>
+									<a href='basic-electrical' className='text-var'>Basic Electrical</a>
+								</li>
+								<li>
+									<a href='cprog' className='text-var'>C Programming</a>
+								</li>
+								<li>
+									<a href='dsa' className='text-var'>Data Structures & ALgorithms</a>
+								</li>
+								<li>
+									<a href='ci' className='text-var'>Constitution of India</a>
+								</li>
+								<li>
+									<a href='envs' className='text-var'>Environmental Science</a>
+								</li>
+							</ul>
+						</p>
+						<hr 
+						style={{	
+						maxWidth: '100%',
+						}}
+						/>
+						<p>
+							<ul style={{
+								listStyleType: 'none',
+							}}>
+								<li><a href='engmath1' className='text-var'>Engineering Maths I</a></li>
+								<li><a href='engmath2' className='text-var'>Engineering Maths II</a></li>
+								<li><a href='engphy' className='text-var'>Engineering Physics</a></li>
+								<li><a href='engchem' className='text-var'>Engineering Chemistry</a></li>
+								<li><a href='engi' className='text-var'>English I</a></li>
+								<li><a href='engii' className='text-var'>English II</a></li>
+								<li>
+									<a href='basic-electronics' className='text-var'>Basic Electronics</a>
+								</li>
+								<li>
+									<a href='basic-electrical' className='text-var'>Basic Electrical</a>
+								</li>
+								<li>
+									<a href='cprog' className='text-var'>C Programming</a>
+								</li>
+								<li>
+									<a href='dsa' className='text-var'>Data Structures & ALgorithms</a>
+								</li>
+								<li>
+									<a href='ci' className='text-var'>Constitution of India</a>
+								</li>
+								<li>
+									<a href='envs' className='text-var'>Environmental Science</a>
+								</li>
+							</ul>
+						</p>
+					</div>
+					</div>
+			</div>
+
 		<Footer />
 		</>
 	)

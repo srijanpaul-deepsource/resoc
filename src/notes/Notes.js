e587b0bf2c2da68528d2d55a1a569652dcde619f
fuzzymfx <anubhabr50@gmail.com>
2023-03-22 00:01:37 +0530

    added search feature

--- src/notes/Notes.js
+++ src/notes/Notes.js
@@ -4,11 +4,60 @@ import { Link } from 'react-router-dom'
 import data from './data.json'
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import Loader from '../Components/Loader'
+import { Search } from 'react-bootstrap-icons';
+import { Form } from 'react-bootstrap'
 
 const storage = getStorage();
 export default function Notes() {
+	const [search, setSearch] = React.useState('')
+	const [displayData, setDisplayData] = React.useState([
+		[{
+			name: '',
+			description: '',
+			links: [
+				{
+					name: '',
+					link: ''
+				}
+			],
+			contributors: [
+				{
+					name: '',
+					link: ''
+				}
+			]
+		},
+		{
+			name: '',
+			description: '',
+			links: [
+				{
+					name: '',
+					link: ''
+				}
+			],
+			contributors: [
+				{
+					name: '',
+					link: ''
+				}
+			]
+		}]
+	])
 	const [loading, setLoading] = React.useState(false)
 	const [syllabus, setSyllabus] = React.useState('')
+	const [isDark, setIsDark] = React.useState(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+	React.useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = (event) => setIsDark(event.matches ? true : false);
+
+    mediaQuery.addEventListener('change', handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleChange);
+    }
+  }, []);
+
 	React.useEffect(() => {
 		setLoading(true)
 		const syllabusRef = ref(storage, `/UG-BTECH-CSE-2018.pdf`);
@@ -19,55 +68,75 @@ export default function Notes() {
 			console.log(error)
 		});
 	}, [])
-				
+	React.useEffect(() => {
+		const searchquery = search.trim().toLowerCase();
+		if (searchquery.length > 0) {
+			setDisplayData(data.filter(item => item.tags.some(tag => tag.toLowerCase().includes(searchquery))))
+		}
+		else {
+			setDisplayData(data)
+		}
+	}, [search])
+
 	return (
 		loading ? <Loader /> :
-		<>
-
-			<section className="py-4 px-sm-3 px-4 cdin">
-				{/* <div className="container "> */}
-				<div className="d-sm-flex align-items-center justify-content-between mainc">
-					<div className="img-home">
-						<h1 className="heading">NOTES<span></span></h1>
-						<p className="lead my-4">
-							Get started here.
-							<br />
-						</p>
-						<a target='_blank'
-				rel="noreferrer"
-				href= {syllabus} className=' text-var'>	B.Tech - Syllabus</a>
+			<>
+				<section className="py-4 px-sm-5 px-4 cdin">
+					{/* <div className="container "> */}
+					<div className="d-sm-flex align-items-center justify-content-between mainc">
+						<div className="img-home">
+							<h1 className="heading">NOTES<span></span></h1>
+							<p className="lead my-4">
+								Get started here.
+								<br />
+							</p>
+							<a target='_blank'
+								rel="noreferrer"
+								href={syllabus} className=' text-var'>	B.Tech - Syllabus</a>
+						</div>
+						<img className="img-fluid w-50 d-none d-sm-block" src={programming} alt="in office" />
 					</div>
-					<img className="img-fluid w-50 d-none d-sm-block" src={programming} alt="in office" />
+					{/* </div> */}
+				</section>
+				<div className='px-1 px-sm-5'>
+					<div className="container px-sm-5">
+					<div className=" d-flex mb-3 justify-content-between">
+          <input style={{
+            userScalable: "no",
+          }} type="text" className="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Search here" onChange={(e) => setSearch(e.target.value)}
+					/>
+          <button type="btn submit" className="btn btn-text-var" style={{
+            background: "none",
+            outline: "dashed 1px",
+            marginLeft: "10px",
+            color: isDark ? "white" : "black", 
+            }}><Search /></button>
+        </div>
+						<ul style={{
+							listStyleType: 'none',
+							padding: '0px',
+						}}>
+							{displayData.map((item, index) => {
+								// console.log(item)
+								return (
+									<li key={index}>
+										<Link to="/previewnotes" state={{
+											name: item.name,
+											description: item.description,
+											links: item.links,
+											contributors: item.contributors
+										}} className='text-var notes-link'>
+											{item.name}
+										</Link>
+									</li>
+								)
+							}
+							)}
+						</ul>
+					</div>
 				</div>
-				{/* </div> */}
-			</section>
-			<div className='px-3 px-sm-5'>
-				<div className="container px-sm-5">
-
-					<ul style={{
-						listStyleType: 'none',
-						padding: '0px',
-					}}>
-						{data.map((item, index) => {
-							return (
-								<li key={index}>
-									<Link to="/previewnotes" state={{
-										name: item.name,
-										description: item.description,
-										links: item.links,
-										contributors: item.contributors
-									}} className='text-var notes-link'>
-										{item.name}
-									</Link>
-								</li>
-							)
-						}
-						)}
-					</ul>
-				</div>
-			</div>
 
 
-		</>
+			</>
 	)
 }

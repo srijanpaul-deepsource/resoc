bdde2ff63cdb844fc804ab5d5e72d69f47295cf1
fuzzymfx <anubhabr50@gmail.com>
2023-03-23 13:53:38 +0530

    update home section

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
 					<p>
 						We have a nifty <Link to="/taskboard" className='text-var'>
 							task section</Link> for you to manage your work.  No more forgetting deadlines and drowning in assignments.
-						Pop over to the <Link to="/community" className='text-var' >community chat page</Link> and give us a holler. We're always happy to help out and connect with like-minded folks. You might even make a new study buddy or two :)
+						Pop over to the <Link to="/community" className='text-var' >community chat page</Link> and give us a holler. We're always happy to help out and connect with like-minded folks. You might even make a new study buddy or two :) You're always welcome to <Link to='/contributions' className='text-var'>contribute notes</Link> to RESOC :)
 					</p>
 					<p>
 						Oh, and if you really dig what we're doing here at RESOC, we'd be over the moon if you'd consider sponsoring us. Just head over to the <Link to="/aboutus" className='text-var'>about section</Link> and donate to our talented content creators. Trust us, it really helps keep the site running and lets us add all sorts of fun new features!

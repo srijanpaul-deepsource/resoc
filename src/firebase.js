83a5b35f0a7b786cbdf7b24dae8687222c1dfb41
fuzzymfx <anubhabr50@gmail.com>
2023-03-25 09:04:45 +0530

    adding GA

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/compat/app'
 import 'firebase/storage';  
 import 'firebase/compat/auth'
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 require('firebase/compat/auth')
 const app = firebase.initializeApp({
@@ -11,7 +12,9 @@ const app = firebase.initializeApp({
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 })
 export const auth = app.auth()
 export default app
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
